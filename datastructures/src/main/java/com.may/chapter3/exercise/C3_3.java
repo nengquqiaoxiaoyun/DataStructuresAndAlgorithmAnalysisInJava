97db120ae9b433403c0af23171606ad52225a95e
thymeleaf <13961809519@163.com>
2020-03-17 09:56:52 +0800

    contains

---
+++ datastructures/src/main/java/com.may/chapter3/exercise/C3_3.java
@@ -0,0 +1,15 @@
+package com.may.chapter3.exercise;
+
+
+/**
+ * 实现MyLinedList的contains方法以下注释摘自LinedList的contains
+ * Returns {@code true} if this list contains the specified element.
+ * More formally, returns {@code true} if and only if this list contains
+ * at least one element {@code e} such that
+ * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>.
+ */
+public class C3_3 {
+
+    // 详情见MyLinedList中的contains
+
+}
